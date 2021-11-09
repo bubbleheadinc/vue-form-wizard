07c6fbbdccab78c0282253eb67b2efc5476f5df0
Samuel Vacca <svacca@pointdynamics.com>
2021-11-09 12:48:53 -0500

    build

--- src/components/TabContent.vue
+++ src/components/TabContent.vue
@@ -52,7 +52,7 @@
         default: () => {}
       }
     },
-    inject: ['addTab', 'removeTab'],
+    // inject: ['addTab', 'removeTab'],
     data () {
       return {
         active: false,
@@ -73,13 +73,13 @@
       }
     },
     mounted () {
-      this.addTab(this)
+      // this.addTab(this)
     },
     destroyed () {
       if (this.$el && this.$el.parentNode) {
         this.$el.parentNode.removeChild(this.$el)
       }
-      this.removeTab(this)
+      // this.removeTab(this)
     }
   }
 </script>
